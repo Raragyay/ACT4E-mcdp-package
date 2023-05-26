4bcbb6344d40d369e225bb93749750a718ebee31
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-05-26 14:19:24 +0200

    misc

--- src/act4e_mcdp/__init__.py
+++ src/act4e_mcdp/__init__.py
@@ -7,6 +7,8 @@ from logging import getLogger, DEBUG
 logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 
+# __version__ = "0.2.1"
+
 from .loading import *
 from .structures import *
 from .main import *
