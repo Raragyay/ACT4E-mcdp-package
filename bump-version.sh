56b49ce8b0c870d5e0d9b834ea82d9ec29cb74a8
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-05-26 14:23:21 +0200

    misc

---
+++ bump-version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Get the latest tag from git
+VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
+
+# Remove the 'v' prefix if your tags use it (like v1.0.0)
+VERSION=${VERSION#v}
+
+# Call bumpversion with the --current-version option
+bumpversion --current-version $VERSION patch
